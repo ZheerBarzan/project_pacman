4a5fc38846f1add0668b44fb861a6ddafcd577d7
Zheer Barzan <zheerbarzan85@outlook.com>
2023-12-18 22:58:47 +0300

    adding the player to the game

---
+++ lib/player.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyPlayer extends StatelessWidget {
+  const MyPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("lib/images/ghost.jpg");
+  }
+}
